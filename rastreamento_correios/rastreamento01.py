45a8b2219beeb23bf50b44f150fa5f5698edd0b8
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-09-16 23:15:58 -0300

    Passa o número de rastreamento por parâmetro
    A inserção do número estava sendo feita dentro do script.
    A passagem por parâmetro permite a execução sem necessidade de
    alterações no código.

--- rastreamento_correios/rastreamento01.py
+++ rastreamento_correios/rastreamento01.py
@@ -1,9 +1,10 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 
 url = 'http://www2.correios.com.br/sistemas/rastreamento/resultado.cfm'
 
-params = { 'objetos': 'codigo_de_rastreamento' }
+params = { 'objetos': sys.argv[1] }
 headers = { 'Referer': 'http://www2.correios.com.br/sistemas/rastreamento/default.cfm' }
 
 html = requests.post(url, data=params, headers=headers)
